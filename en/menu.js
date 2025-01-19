2cc35e9aad0ca37e4cba3879240811866e8f5f33
3500cm2 <3500cm2@gmail.com>
2025-01-19 11:03:36 +0100

    Create menu.js

---
+++ en/menu.js
@@ -0,0 +1,9 @@
+document.write(`
+<div class="menu" id="menu">
+    <a href="index.html">home</a>
+    <a href="pagina.html">artists</a>
+    <a href="news.html">news</a>
+    <a href="luoghi.html">venues</a>
+    <a href="contatti.html">cantact</a>
+</div>
+`);
