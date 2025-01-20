a95bb71a356903e871ab5dcd7e9de3b5ed9c2ab9
3500cm2 <3500cm2@gmail.com>
2025-01-20 22:43:45 +0100

    Create menu.js

---
+++ it/menu.js
@@ -0,0 +1,7 @@
+document.getElementById('menu').innerHTML = `
+    <a href="index.html">home</a>
+    <a href="pagina.html">artisti</a>
+    <a href="news.html">news</a>
+    <a href="luoghi.html">luoghi</a>
+    <a href="contatti.html">contatti</a>
+`;
