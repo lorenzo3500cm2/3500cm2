6a87f55b5f258b214557f0b4c217cb41c6193db1
3500cm2 <3500cm2@gmail.com>
2025-01-29 13:17:49 +0100

    Update menu.js

--- en/menu.js
+++ en/menu.js
@@ -2,7 +2,7 @@ document.getElementById('menu').innerHTML = `
     <a href="index.html">home</a>
     <a href="pagina.html">artists</a>
     <a href="news.html">news</a>
-    <a href="location.html">venues</a>
+    <a href="locations.html">venues</a>
     <a href="contact.html">contact</a>
 `;
 
