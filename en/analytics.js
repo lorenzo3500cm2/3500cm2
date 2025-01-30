0985724f69660249a9eb953c0184fd394d38398d
3500cm2 <3500cm2@gmail.com>
2025-01-31 00:34:09 +0100

    Create analytics.js

---
+++ en/analytics.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX";
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XXXXXXXXXX'); // Sostituisci con il tuo ID
+})();
