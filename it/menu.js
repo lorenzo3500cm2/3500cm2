d907f473628695d02293701ccb728ba492bbf79b
3500cm2 <3500cm2@gmail.com>
2025-03-25 08:51:56 +0100

    Update menu.js

--- it/menu.js
+++ it/menu.js
@@ -3,5 +3,6 @@ document.getElementById('menu').innerHTML = `
     <a href="pagina.html">artisti</a>
     <a href="news.html">news</a>
     <a href="luoghi.html">luoghi</a>
+    <a href="partner.html">partner</a>
     <a href="contatti.html">contatti</a>
 `;
