ff8a3f330d688852db1d07d6a2d5058570564b48
3500cm2 <3500cm2@gmail.com>
2025-03-25 08:52:20 +0100

    Update menu.js

--- en/menu.js
+++ en/menu.js
@@ -3,7 +3,7 @@ document.getElementById('menu').innerHTML = `
     <a href="pagina.html">artists</a>
     <a href="news.html">news</a>
     <a href="locations.html">venues</a>
-    <a href="partner.html">contact</a>
+    <a href="partner.html">partner</a>
     <a href="contact.html">contact</a>
 `;
 
